1711a3207a9ad304318b6bf75e2c0be0dcfca7d7
Jakob Friberg <ollepakvarn@gmail.com>
2026-02-20 10:36:26 +0100

    Update sha256 for speqs 0.1.0

--- Formula/speqs.rb
+++ Formula/speqs.rb
@@ -2,7 +2,7 @@ class Speqs < Formula
   desc "The command-line interface for Speqs"
   homepage "https://speqs.io"
   url "https://registry.npmjs.org/speqs/-/speqs-0.1.0.tgz"
-  sha256 "PLACEHOLDER"
+  sha256 "863b00787c6b4b929a617944441f58c8362f302b742c3177e94a774d26c69123"
   license :cannot_represent
 
   depends_on "node"
